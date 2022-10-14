d6102e7a08b47a9e0bd941ef2fe2fb88676b90f4
ukaemma2 <ukaemmanuel2000@gmail.com>
2022-10-13 17:58:32 -0700

    fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - prints the phrase 
-* "Programming is like building a  multilingual pizzle"
+* main - prints the phrase "Programming is like building a imultilingual pizzle"
 * Return: 0 if exited properly, non-zer0 otherwise
 */
 
