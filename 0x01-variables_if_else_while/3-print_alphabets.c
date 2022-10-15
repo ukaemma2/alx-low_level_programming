899ffc7b8d777e82626f4ef47cdfbf2747d82e57
ukaemma2 <ukaemmanuel2000@gmail.com>
2022-10-15 12:43:05 -0700

    3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase
+ * main - prints the alphabet in lowercase, and then in uppercase
  * You can only use the putchar function (every other function
  * (printf, puts, etc…) is forbidden)
  * All your code should be in the main function
- * You can only use putchar twice in your code
+ * You can only use putchar three times in your code
  * Return: Always Success
  */
 
 int main(void)
 {
-	int gh;
+	int ch;
 
-
-	for (gh = 'a'; gh <= 'z'; gh++)
-	{
-		putchar(gh);
-	}
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
 	putchar('\n');
 	return (0);
 }
-
