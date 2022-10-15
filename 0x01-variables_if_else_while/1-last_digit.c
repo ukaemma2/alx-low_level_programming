c94b4d87b113dbf7fd767e7cd84cc0bf33aab7af
ukaemma2 <ukaemmanuel2000@gmail.com>
2022-10-15 11:43:16 -0700

    fixed Betty stlyles

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,6 +7,7 @@
  * if the last digit of n is greater than 5
  * if the last digit of n is 0: the string and is 0
  * if the last digit of n is less than 6 and not 0: the string
+ * Return: Always (Success) 0
  */
 
 int main(void)
