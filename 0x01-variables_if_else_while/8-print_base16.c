24fc9058a82a6b6090d0761c0d438d9ca6eec985
ukaemma2 <ukaemmanuel2000@gmail.com>
2022-10-15 18:22:54 -0700

    9-print_comb

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints all the numbers of base 16 in lowercase
- * You can only use the putchar function 
+ * You can only use the putchar function
  * All your code should be in the main function
  * You can only use putchar three times in your code
  * Return: Always Success 0
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ *
+ * Return: Success
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + '0');
+
+		if(n != 10)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
