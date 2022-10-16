7c887e46ffc04b489f91e3439394ef50982b086e
ukaemma2 <ukaemmanuel2000@gmail.com>
2022-10-15 19:23:34 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits.
+ *
+ * Return: Always Success (0)
+ */
+
+int main(void)
+{
+	int m, n;
+
+	for (m = 48; m <= 56; m++)
+	{
+		for (n = 49; n <= 57; n++)
+		{
+			if (n > m)
+			{
+				putchar(m);
+				putchar(n);
+				if (m !=56 || n != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,7 +14,7 @@ int main(void)
 	{
 		putchar(n + '0');
 
-		if(n != 9)
+		if (n != 9)
 		{
 			putchar(',');
 			putchar(' ');
