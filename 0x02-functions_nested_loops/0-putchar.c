c90ed828a0d9660a6480b0ae16cf2643c81ee455
ukaemma2 <ukaemmanuel2000@gmail.com>
2022-10-18 12:13:26 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Write a program that prints _putchar, followed by a new line
+ *
+ * Return: Always Success 
+ */
+
+int main(void)
+{
+	_putchar('h');
+	return (0);
+}	
+
