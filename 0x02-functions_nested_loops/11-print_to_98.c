df3244df015bd271f79084e6a15cb98e042984c5
ukaemma2 <ukaemmanuel2000@gmail.com>
2022-10-18 20:26:34 -0700

    100-test_table

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_to_98(3);
+    _putchar('\n');
+    print_to_98(5);
+    _putchar('\n');
+    print_to_98(98);
+    _putchar('\n');
+    print_to_98(12);  
+    return (0);
+}
+
+
