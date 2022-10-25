4814f8e32290f206e7532b8e684caf2581b14b5f
ukaemma2 <ukaemmanuel2000@gmail.com>
2022-10-25 13:03:04 -0700

    101-keygen

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -13,8 +13,10 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 void print_rev(char *s);
 int _strlen(char *s);
+int _atoi(char *s);
 void rev_string(char *s);
 int _putchar(char c);
 void swap_int(int *a, int *b);
+char *_strcpy(char *dest, char *src);
 void reset_to_98(int *n);
 #endif
