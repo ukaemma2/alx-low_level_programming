708d889328bf53c5243682d361839538d37b4b53
ukaemma2 <ukaemmanuel2000@gmail.com>
2022-10-25 11:39:11 -0700

    5-rev_string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,6 +10,7 @@
 void _puts(char *str);
 void print_rev(char *s);
 int _strlen(char *s);
+void rev_string(char *s);
 int _putchar(char c);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
