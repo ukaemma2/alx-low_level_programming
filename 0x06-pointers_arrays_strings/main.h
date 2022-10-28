2ee6fa4fd39abf4ae03aa19033dbabf4573b6c48
ukaemma2 <ukaemmanuel2000@gmail.com>
2022-10-28 09:36:26 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
+void print_number(int n);
 char *_strcat(char *dest, char *src);
-
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+int _putchar(char c);
+ char *leet(char *);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+char *_strncat(char *dest, char *src, int n);
 #endif /**MAIN_H*/
