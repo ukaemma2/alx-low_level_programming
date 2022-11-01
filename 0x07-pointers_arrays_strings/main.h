8171124e850b3b83f8982ec914f4d980db98fbf4
ukaemma2 <ukaemmanuel2000@gmail.com>
2022-11-01 13:06:55 -0700

    memcpy

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 char *_memset(char *s, char b, unsigned int n);
 void simple_print_buffer(char *buffer, unsigned int size);
-
+char *_memcpy(char *dest, char *src, unsigned int n);
+int _putchar(char c);
 
 #endif /*MAIN_H*/
