59c039643059deeb76681b8d2e6a56cf11c54cf0
ukaemma2 <ukaemmanuel2000@gmail.com>
2022-11-01 13:33:11 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 char *_memset(char *s, char b, unsigned int n);
+unsigned int _strspn(char *s, char *accept);
 void simple_print_buffer(char *buffer, unsigned int size);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
