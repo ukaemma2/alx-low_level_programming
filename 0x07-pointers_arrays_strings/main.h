b6875095f7ad8123a3d595c3067251de597e76cb
ukaemma2 <ukaemmanuel2000@gmail.com>
2022-11-02 04:46:36 -0700

    update

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ char *_memset(char *s, char b, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
 void simple_print_buffer(char *buffer, unsigned int size);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strpbrk(char *s, char *accept);
 char *_strchr(char *s, char c);
 int _putchar(char c);
 
