63fad73b35e1bf79b22f5f25d3c3654daa38ae57
ukaemma2 <ukaemmanuel2000@gmail.com>
2022-11-02 08:02:03 -0700

    set

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void print_chessboard(char (*a)[8]);
 char *_memset(char *s, char b, unsigned int n);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 char *_strstr(char *haystack, char *needle);
 unsigned int _strspn(char *s, char *accept);
 void simple_print_buffer(char *buffer, unsigned int size);
