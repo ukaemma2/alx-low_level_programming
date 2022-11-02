78e27f8706159bc25138ccb2cc88c3d4704fdbf4
ukaemma2 <ukaemmanuel2000@gmail.com>
2022-11-02 11:21:16 -0700

    5-sqrt_recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,6 +4,7 @@
 int _pow_recursion(int x, int y);
 int factorial(int n);
 int _strlen_recursion(char *s);
+int _sqrt_recursion(int n);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _putchar(char c);
