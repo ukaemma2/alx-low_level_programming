41aa37713664672e1b773faf5e558fcf328017b0
ukaemma2 <ukaemmanuel2000@gmail.com>
2022-11-02 07:36:11 -0700

    5-strstr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 char *_memset(char *s, char b, unsigned int n);
+char *_strstr(char *haystack, char *needle);
 unsigned int _strspn(char *s, char *accept);
 void simple_print_buffer(char *buffer, unsigned int size);
 char *_memcpy(char *dest, char *src, unsigned int n);
