0d405d22e2ab53c82896d9c1abfa50c5fef52c1a
ukaemma2 <ukaemmanuel2000@gmail.com>
2022-11-02 11:29:19 -0700

    6-is_prime_number.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,6 +7,7 @@ int _strlen_recursion(char *s);
 int _sqrt_recursion(int n);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
+int is_prime_number(int n);
 int _putchar(char c);
 #endif /*MAIN_H*/
 
