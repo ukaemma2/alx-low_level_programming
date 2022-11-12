02458302198bafbf2ee4323ce82f219c44d1feed
ukaemma2 <ukaemmanuel2000@gmail.com>
2022-11-12 13:38:36 -0800

    alloc_gride

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,7 @@
 #ifndef _MAIN_H
 #define _MAIN_H
+void free_grid(int **grid, int height);
+int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
