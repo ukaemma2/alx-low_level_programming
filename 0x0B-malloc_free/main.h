e5a9b8acf613ce77045993d16d91260d59d72722
ukaemma2 <ukaemmanuel2000@gmail.com>
2022-11-12 14:20:23 -0800

    101-strtow

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,8 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
 char *create_array(unsigned int size, char c);
